43f252dddf8dde653e6fd954827b027e41d7a733
nikitachoudhary114 <nikitachoudhary364@gmail.com>
2025-10-05 14:47:42 +0530

    minor change

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -69,6 +69,7 @@ app.post("/todo", (req, res) => {
 app.get("/todo", (req, res) => {
   const { userId } = req.query; // string
 
+  
   prismaClient.todo
     .findMany({
       where: userId ? { userId: String(userId) } : {}, // if no userId, return all todos
